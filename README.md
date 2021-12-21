f609f2c65cfb2b25d2ff0c78d19db7b784c03742
Sofarz <sofia.wallen@gmail.com>
2021-12-21 09:51:47 +0100

    code clean up

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # foodora-case
+
+This is a project created for a case at foodora. I created it using HTML, CSS and JavaScript.
+
+I made it for only desktop view, since I did not have the time to make it responsive. If I where to have more time I would have styled it for mobile first, and then add media queries for tablet, small desktop and large desktop.
+
+It has also only been tested for Chrome.
--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+// Selecting elements and assigning them to variables 
 const btnContainer = document.getElementById('order-online-button-container')
 const showMenuBtn = document.getElementById('show-menu-btn')
 const burgerMenu = document.getElementById('burger-menu')
 const closeMenu = document.getElementById('close-menu')
 
+/* SETUP event listeners. When clicking on order online btn, show menu. When clicking 
+on window close, hide menu and show order online btn again. */
 showMenuBtn.addEventListener('click', () => {
     burgerMenu.classList.add('burger-menu-selected')
     burgerMenu.classList.remove('close-menu')
